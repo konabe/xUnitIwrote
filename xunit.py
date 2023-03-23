577d1eebf04a6a3eac34676c5d14e9381b37a6a5
Ryota Konabe <rt.konabe@gmail.com>
2023-03-23 22:27:05 +0900

    setUp関数のテストコード追加

--- xunit.py
+++ xunit.py
@@ -1,3 +1,4 @@
+# テストメソッドを動的に呼び出す
 class TestCase:
     def __init__(self, name):
         self.name = name
@@ -5,12 +6,15 @@ class TestCase:
         method = getattr(self, self.name)
         method()
         
+# メソッドが起動されたかを記録する
 class WasRun(TestCase):
     def __init__(self, name):
         self.wasRun = None
         super().__init__(name)
     def testMethod(self):
         self.wasRun = 1
+
+# テストコード
 
 class TestCaseTest(TestCase):
     def testRunning(self):
@@ -19,4 +23,11 @@ class TestCaseTest(TestCase):
         test.run()
         assert(test.wasRun)
 
+    def testSetUp(self):
+        test = WasRun("testMethod")
+        test.run()
+        assert(test.wasSetUp)
+        
+
 TestCaseTest("testRunning").run()
+TestCaseTest("testSetUp").run()
