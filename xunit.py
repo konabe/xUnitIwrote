0c56af905ddf869baf856c18344b48bb5118aa89
Ryota Konabe <rt.konabe@gmail.com>
2023-03-23 22:49:56 +0900

    setUp関数の実装
    （呼び出しはTestCaseで、記録はWasRunに分離することを意識しながら）

--- xunit.py
+++ xunit.py
@@ -2,7 +2,10 @@
 class TestCase:
     def __init__(self, name):
         self.name = name
+    def setUp(self):
+        pass
     def run(self):
+        self.setUp()
         method = getattr(self, self.name)
         method()
         
@@ -11,6 +14,8 @@ class WasRun(TestCase):
     def __init__(self, name):
         self.wasRun = None
         super().__init__(name)
+    def setUp(self):
+        self.wasSetUp = 1
     def testMethod(self):
         self.wasRun = 1
 
