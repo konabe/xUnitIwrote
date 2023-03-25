c0faedc37936cb75d02cd38514b536e2999f2699
Ryota Konabe <rt.konabe@gmail.com>
2023-03-25 15:33:26 +0900

    メソッドの実行をreflectionで隠蔽

--- xunit.java
+++ xunit.java
@@ -1,21 +1,28 @@
+import java.lang.reflect.Method;
+import java.lang.reflect.InvocationTargetException;
+
 class WasRun {
   Integer wasRun;
+  private String _name;
 
   WasRun(String name) {
     wasRun = null;
+    _name = name;
   }
 
-  void run() {
-    testMethod();
+   void run() throws ClassNotFoundException, IllegalAccessException, NoSuchMethodException, InvocationTargetException {
+    // getMethodはpublicを宣言しないと見つけてくれない
+    Method method = this.getClass().getMethod(_name);
+    method.invoke(this);
   }
 
-  void testMethod() {
+  public void testMethod() {
     wasRun = 1;
   }
 }
 
 class Main {
-  public static void main(String[] args) {
+  public static void main(String[] args) throws ClassNotFoundException, IllegalAccessException, NoSuchMethodException, InvocationTargetException {
     WasRun test = new WasRun("testMethod");
     System.out.println(test.wasRun);
     test.run();
